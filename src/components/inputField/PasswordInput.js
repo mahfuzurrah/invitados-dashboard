b1faa5a642a252a3cb57b6138978bfbcf08dda91
mahfuzurrah <mahfuzuruiux@gmail.com>
2024-04-15 07:26:52 +0600

    Add

--- src/components/inputField/PasswordInput.js
+++ src/components/inputField/PasswordInput.js
@@ -1,8 +1,8 @@
-import React, { useState } from 'react';
-import { FiEye, FiEyeOff } from 'react-icons/fi';
+import React, { useState } from "react";
+import { FiEye, FiEyeOff } from "react-icons/fi";
 
-const PasswordInput = ({ label, star, value, onChange, error }) => {
-  const inputId = label.replace(/\s+/g, '-').toLowerCase();
+const PasswordInput = ({ label, star, value, onChange, error, placeholder }) => {
+  const inputId = label.replace(/\s+/g, "-").toLowerCase();
   const [showPassword, setShowPassword] = useState(false);
 
   const handleTogglePassword = () => {
@@ -10,22 +10,20 @@ const PasswordInput = ({ label, star, value, onChange, error }) => {
   };
 
   return (
-    <div className='mt-3 w-full'>
-      <label htmlFor={inputId} className='mb-2' key={inputId}>
-        {label} <span className='input_star'>{star}</span>
+    <div className="mt-3 w-full">
+      <label htmlFor={inputId} className="mb-2" key={inputId}>
+        {label} <span className="input_star">{star}</span>
       </label>
       <div className="password-input-container">
         <input
           id={inputId}
-          type={showPassword ? 'text' : 'password'}
+          type={showPassword ? "text" : "password"}
           value={value}
+          placeholder={placeholder}
           onChange={onChange}
           required
         />
-        <span
-          className="password-toggle-icon"
-          onClick={handleTogglePassword}
-        >
+        <span className="password-toggle-icon" onClick={handleTogglePassword}>
           {showPassword ? <FiEye /> : <FiEyeOff />}
         </span>
       </div>
