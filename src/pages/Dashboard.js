ab90367042a058791597b4974f6af17bb40c12d0
mahfuzurrah <mahfuzuruiux@gmail.com>
2024-04-15 08:55:51 +0600

    add

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -44,7 +44,7 @@ function Dashboard() {
             <div className="col-lg-8 col-md-7 col-sm-12">
               <div className="card">
                 <div className="text">
-                  <p>Próximos Eventos</p>
+                  <p>Cuenta regresiva</p>
                   <div className="time">
                     <h1>{countdown.days}</h1> d<h1>{countdown.hours}</h1> h
                     <h1>{countdown.minutes}</h1> m<h1>{countdown.seconds}</h1> s
@@ -56,7 +56,7 @@ function Dashboard() {
             <div className="col-lg-4 col-md-5 col-sm-12">
               <div className="card">
                 <div className="text">
-                  <p>Invitación aceptada</p>
+                  <p>Invitados confirmados</p>
                   <h1>500</h1>
                 </div>
                 <img src={user} alt="Icon" />
@@ -65,7 +65,7 @@ function Dashboard() {
           </div>
           <div className="table_area mt-3">
             <div className="table_header">
-              <h2>Confirmaciones recientess</h2>
+              <h2>Confirmaciones recientes</h2>
               <Link to="/guests_list">
                 Ver todo
                 <GrFormNextLink className="icons" />
